adb01cb51bd60d83eebbb0835113744336479450
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-11-01 09:53:47 -0600

    plz

---
+++ Server/index.js
@@ -0,0 +1,1 @@
+let ctrl = require("./controller");
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+export default class Form extends Component {
+  render() {
+    return <div>Form</div>;
+  }
+}
